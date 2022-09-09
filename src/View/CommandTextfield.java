00161ad4663062f05107c3bc1eaeef91508437c7
posice <ilayyasi@gmail.com>
2022-09-09 22:11:51 +0200

    bisschen textfield dies das

--- src/View/CommandTextfield.java
+++ src/View/CommandTextfield.java
@@ -1,4 +1,35 @@
 package View;
 
-public class CommandTextfield {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class CommandTextfield extends JTextField implements KeyListener{
+    public CommandTextfield(){
+        Border border = BorderFactory.createLineBorder(Color.white);
+        this.setHorizontalAlignment(JTextField.LEFT);
+        this.setForeground(Color.WHITE);
+        this.setCaretColor(Color.WHITE);
+        this.setBackground(new Color(89,47,34));
+        this.setBounds(0,0,540,40);
+        this.setBorder(border);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
--- src/View/Frame.java
+++ src/View/Frame.java
@@ -2,8 +2,10 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Frame extends JFrame {
+public class Frame extends JFrame implements KeyListener {
 
     public Frame(){
         this.setSize(1080,600);
@@ -16,5 +18,24 @@ public class Frame extends JFrame {
 
         ImageIcon imageIcon = new ImageIcon("TATE.webp");
         this.setIconImage(imageIcon.getImage());
+
+        CommandTextfield commandTextfield = new CommandTextfield();
+        commandTextfield.addKeyListener(this);
+        this.add(commandTextfield);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
