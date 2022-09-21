93cbec4304d119edc9e9ed450c10c22b15cd031f
posice <ilayyasi@gmail.com>
2022-09-21 13:10:19 +0200

    infocontroller

--- src/view/OutputLabel.java
+++ src/view/OutputLabel.java
@@ -5,9 +5,6 @@ import java.awt.*;
 
 public class OutputLabel extends JLabel {
     public OutputLabel(){
-        this.setBackground(Color.green);
-        this.setForeground(Color.cyan);
-        this.setFont(new Font("Uni Sans", Font.PLAIN, 13));
         this.setText("siu");
     }
 
