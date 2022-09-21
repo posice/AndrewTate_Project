a190eb450ebe91fef724f4eccec030f89fb57dab
Paul Stych <stycpaul@kkg-do.eu>
2022-09-21 12:51:02 +0200

    neuer RoomController

--- src/control/RoomController.java
+++ src/control/RoomController.java
@@ -1,4 +1,18 @@
 package control;
+import model.Room;
 
 public class RoomController {
+
+    private Room mansion;
+    private Room frenchBrothel;
+    private Room gym;
+    private Room garage;
+
+    public RoomController() {
+        mansion = new Room("mansion", "In the mansion there is Tristan waiting for you");
+        frenchBrothel = new Room("frenchBrothel", "Ooooo, a very weird place, a lot of chicks and strange French" +
+                                    "men who are jerking off");
+        gym = new Room("gym", "It's time to train, a lot of TopGs in here");
+        garage = new Room("garage", "The cars" );
+    }
 }
