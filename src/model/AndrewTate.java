85f31c55a6343a1ab50cf6bc457501f5e0c37d44
Paul Stych <stycpaul@kkg-do.eu>
2022-09-21 12:59:10 +0200

    AndrewTate update

--- src/model/AndrewTate.java
+++ src/model/AndrewTate.java
@@ -1,16 +1,19 @@
 package model;
 
-public class AndrewTate extends Person {
+public class AndrewTate {
 
+    private double money;
+    private int follower;
+    private Room location;
     private Car[] cars;
 
-    public AndrewTate(String name, double money, Room location, int bodyCount, int age) {
-        super(name,money,location,bodyCount,age);
+    public AndrewTate() {
+        money = 0;
+        follower = 0;
     }
 
-    public double getMoney(){return money;}
-
-    public int getBodyCount(){return bodyCount;}
-
-    public int getAge(){return age;}
+    public double getMoney(){ return money; }
+    public int getFollower() { return follower;}
+    public Room getLocation() { return location; }
+    public void setLocation(Room location) { this.location = location; }
 }
