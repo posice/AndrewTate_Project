0d22eb1a047339bfa4e4e8bd1a27e46cfb39b95d
Paul Stych <stycpaul@kkg-do.eu>
2022-09-23 09:24:05 +0200

    Änderungen am RoomController

--- src/control/RoomController.java
+++ src/control/RoomController.java
@@ -16,37 +16,63 @@ public class RoomController {
     private Room currentRoomTristan;
 
     public RoomController() {
-        mansion = new Room("mansion", "");
+        mansion = new Room("mansion", "", new String[]{"chick", "Tristan"});
         currentRoomAndrew = mansion;
         currentRoomTristan = mansion;
-        frenchBrothel = new Room("french brothel", "");
-        gym = new Room("gym", "");
-        garage = new Room("garage", "" );
-        bugattiStore = new Room("Bugatti store", "");
-        lamboStore = new Room("Lambo store", "");
-        astonMartinStore = new Room("Aston Martin store", "");
-        ferrariStore = new Room("Ferrari store", "");
-        opelStore = new Room("Opel store", "");
+        frenchBrothel = new Room("french brothel", "", new String[]{"chick", "NineFive"});
+        gym = new Room("gym", "", new String[]{"G"});
+        garage = new Room("garage", "", new String[]{});
+        bugattiStore = new Room("Bugatti store", "", new String[]{"G", "chick"});
+        lamboStore = new Room("Lambo store", "", new String[]{"G"});
+        astonMartinStore = new Room("Aston Martin store", "", new String[]{"G", "chick"});
+        ferrariStore = new Room("Ferrari store", "", new String[]{"G"});
+        opelStore = new Room("Opel store", "", new String[]{"NinveFive"});
     }
 
     public Room getCurrentRoomAndrew() { return currentRoomAndrew; }
     public Room getCurrentRoomTristan() { return currentRoomTristan; }
 
-    public void setCurrentRoomTristan (Room newRoom) { currentRoomTristan = newRoom; }
+    //public void setCurrentRoomTristan (Room newRoom) { currentRoomTristan = newRoom; }
     public void setCurrentRoomAndrew(Room newRoom) { currentRoomAndrew = newRoom; }
 
-    public void setRandomCurrentRoom() {
+    public void setRandomCurrentRoomTristan() {
         int randomN = (int)(Math.random()*100);
         if (randomN < 40) {
+            currentRoomTristan.setTristanOutRoom();
+            mansion.setTristanInRoom();
             currentRoomTristan = mansion;
         } else if (randomN < 44) {
+            currentRoomTristan.setTristanOutRoom();
+            frenchBrothel.setTristanInRoom();
             currentRoomTristan = frenchBrothel;
         } else if (randomN < 70) {
+            currentRoomTristan.setTristanOutRoom();
+            gym.setTristanInRoom();
             currentRoomTristan = gym;
         } else if (randomN < 80) {
+            currentRoomTristan.setTristanOutRoom();
+            garage.setTristanInRoom();
             currentRoomTristan = garage;
         } else if (randomN < 85) {
+            currentRoomTristan.setTristanOutRoom();
+            bugattiStore.setTristanInRoom();
             currentRoomTristan = bugattiStore;
+        } else if (randomN < 89) {
+            currentRoomTristan.setTristanOutRoom();
+            lamboStore.setTristanInRoom();
+            currentRoomTristan = lamboStore;
+        } else if (randomN < 93) {
+            currentRoomTristan.setTristanOutRoom();
+            astonMartinStore.setTristanInRoom();
+            currentRoomTristan = astonMartinStore;
+        } else if (randomN < 97) {
+            currentRoomTristan.setTristanOutRoom();
+            ferrariStore.setTristanInRoom();
+            currentRoomTristan = ferrariStore;
+        } else if (randomN < 98) {
+            currentRoomTristan.setTristanOutRoom();
+            opelStore.setTristanInRoom();
+            currentRoomTristan = opelStore;
         }
     }
 }
--- src/model/Room.java
+++ src/model/Room.java
@@ -4,12 +4,31 @@ public class Room {
 
     private String name;
     private String info;
+    private String[] personsInRoom;
 
-    public Room(String name, String info) {
+    public Room(String name, String info, String[] personsInRoom) {
         this.name = name;
         this.info = info;
+        this.personsInRoom = personsInRoom;
     }
 
     public String getName() { return name; }
     public String getInfo() { return info; }
+    public String[] getPersonsInRoom() { return personsInRoom; }
+
+    public void setTristanInRoom() {
+        String[] newPINR = new String[personsInRoom.length + 1];
+        for (int i = 0; i < personsInRoom.length; i++) {
+            newPINR[i] = personsInRoom[i];
+        }
+        newPINR[newPINR.length - 1] = "Tristan";
+        personsInRoom = newPINR;
+    }
+
+    public void setTristanOutRoom() {
+        String[] newPINR = new String[personsInRoom.length - 1];
+        for (int i = 0; i < newPINR.length; i++) {
+            newPINR[i] = personsInRoom[i];
+        }
+    }
 }
