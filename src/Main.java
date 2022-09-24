a22200f4e32bd5c4ad5515dc5d4977abab0ed460
posice <ilayyasi@gmail.com>
2022-09-24 13:27:42 +0200

    some changes in frames and stuff

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import control.MainController;
 import model.tf.PredictCmd;
 import view.Frame;
+import view.WinningFrame;
 
 public class Main {
     static MainController mc = new MainController();
-    public static void main(String[] args) { mc.control(); }
+    public static void main(String[] args) {mc.control();}
 }
